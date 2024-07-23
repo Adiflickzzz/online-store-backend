bf453ca5cab74b1ca2c2c488dab43830b1495ca3
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-07-23 15:12:40 +0530

    order.js updated

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -37,7 +37,7 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         shipping_address_collection: { allowed_countries: ["IN"] },
         payment_method_types: ["card"],
         mode: "payment",
-        success_url: process.env.CLIENT_URL + `/success`,
+        success_url: process.env.CLIENT_URL + "/success",
         cancel_url: process.env.CLIENT_URL + "/failed",
         line_items: lineItems,
       });
